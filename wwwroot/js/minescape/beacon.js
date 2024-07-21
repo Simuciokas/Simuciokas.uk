bcc85f51dc9b5747bf126ad2dae4f9b8e2e63339
Simuciokas <maxsimass@gmail.com>
2024-07-21 17:58:51 +0300

    iframe for map

--- wwwroot/js/minescape/beacon.js
+++ wwwroot/js/minescape/beacon.js
@@ -55,10 +55,12 @@ function autocomplete(inp, arr) {
                     let z = location.split(", ")[2]
                     let tip = beacons[inp.value].tip
 
-                    let text = `https://map.minescape.net/#/${x}/${y}/${z}/-1/minescape/minescape`
+                    let text = `https://map.minescape.net/#/${x}/${y}/${z}/-2/minescape/minescape`
                     document.getElementById("solution-beacon-tip").innerText = tip;
                     document.getElementById("solution-beacon-url").href = text;
                     document.getElementById("solution-beacon-url").innerText = location;
+                    document.getElementById("solution-beacon-map").innerHTML = ""
+                    document.getElementById("solution-beacon-map").innerHTML = `<iframe id=\"\" style=\"width:100%; height:650px;\" src=\"${text}\"></iframe>`
 
                     /*close the list of autocompleted values,
                     (or any other open lists of autocompleted values:*/
@@ -104,14 +106,11 @@ function autocomplete(inp, arr) {
         x[currentFocus].classList.add("autocomplete-active");
     }
     function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/
         for (var i = 0; i < x.length; i++) {
             x[i].classList.remove("autocomplete-active");
         }
     }
     function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-        except the one passed as an argument:*/
         var x = document.getElementsByClassName("autocomplete-items");
         for (var i = 0; i < x.length; i++) {
             if (elmnt != x[i] && elmnt != inp) {
@@ -119,7 +118,7 @@ function autocomplete(inp, arr) {
             }
         }
     }
-    /*execute a function when someone clicks in the document:*/
+
     document.addEventListener("click", function (e) {
         closeAllLists(e.target);
     });
--- wwwroot/js/minescape/cypher.js
+++ wwwroot/js/minescape/cypher.js
@@ -8,8 +8,6 @@ async function Startup() {
 
     if (response.ok)
         anagrams = await response.json()
-    else
-        console.error("Cannot load")
 
     document.getElementById("CypherInput").addEventListener("input", function (e, val) {
         let cipher = e.target.value.toLowerCase()
@@ -22,17 +20,18 @@ async function Startup() {
             let shifted = caesarShift(cipher, i)
             for (let j = 0; j < ciphers.length; j++) {
                 if (anagrams[ciphers[j]].id == shifted) {
-                    found = anagrams[ciphers[j]];
-                    break;
+                    found = anagrams[ciphers[j]]
+                    break
                 }
             }
         }
 
         if (found == null) {
-            document.getElementById("solution-cypher-name").innerText = "not found";
-            document.getElementById("solution-cypher-tip").innerText = "not found";
-            document.getElementById("solution-cypher-url").href = "";
-            document.getElementById("solution-cypher-url").innerText = "not found";
+            document.getElementById("solution-cypher-name").innerText = "not found"
+            document.getElementById("solution-cypher-tip").innerText = "not found"
+            document.getElementById("solution-cypher-url").href = ""
+            document.getElementById("solution-cypher-url").innerText = "not found"
+            document.getElementById("solution-cypher-map").innerHTML = ""
         }
         else {
 
@@ -44,50 +43,41 @@ async function Startup() {
             let z = location.split(", ")[2]
             let tip = found.tip
 
-            let text = `https://map.minescape.net/#/${x}/${y}/${z}/-1/minescape/minescape`
-            document.getElementById("solution-cypher-tip").innerText = tip;
-            document.getElementById("solution-cypher-name").innerText = name == undefined ? "No name provided" : name;
-            document.getElementById("solution-cypher-url").href = text;
-            document.getElementById("solution-cypher-url").innerText = location;
+            let text = `https://map.minescape.net/#/${x}/${y}/${z}/-2/minescape/minescape`
+            document.getElementById("solution-cypher-tip").innerText = tip
+            document.getElementById("solution-cypher-name").innerText = name == undefined ? "No name provided" : name
+            document.getElementById("solution-cypher-url").href = text
+            document.getElementById("solution-cypher-url").innerText = location
+            document.getElementById("solution-cypher-map").innerHTML = ""
+            document.getElementById("solution-cypher-map").innerHTML = `<iframe id=\"\" style=\"width:100%; height:650px;\" src=\"${text}\"></iframe>`
         }
     });
 }
 
 function caesarShift(str, amount) {
-    // Wrap the amount
     if (amount < 0) {
         return caesarShift(str, amount + 26);
     }
 
-    // Make an output variable
     var output = "";
 
-    // Go through each character
     for (var i = 0; i < str.length; i++) {
-        // Get the character we'll be appending
         var c = str[i];
 
-        // If it's a letter...
         if (c.match(/[a-z]/i)) {
-            // Get its code
             var code = c.codePointAt();
 
-            // Uppercase letters
             if (code >= 65 && code <= 90) {
                 c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
             }
 
-            // Lowercase letters
             else if (code >= 97 && code <= 122) {
                 c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
             }
         }
 
-        // Append
         output += c;
     }
-
-    // All done!
     return output;
 };
 
