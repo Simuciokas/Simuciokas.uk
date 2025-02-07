047b5e7ac08535fcc7b6a749fb2a2c3c2ba1b48a
Simuciokas <maxsimass@gmail.com>
2025-02-07 23:11:44 +0200

    2025 and handle apostrophe

--- wwwroot/js/minescape/chest.js
+++ wwwroot/js/minescape/chest.js
@@ -33,12 +33,12 @@ function autocomplete(inp, arr) {
                 b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
                 b.innerHTML += arr[i].substr(val.length);
                 /*insert a input field that will hold the current array item's value:*/
-                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.innerHTML += "<input type=\"hidden\" value=\"" + arr[i] + "\">";
                 /*execute a function when someone clicks on the item value (DIV element):*/
                 b.addEventListener("click", function (e) {
                     /*insert the value for the autocomplete text field:*/
                     inp.value = this.getElementsByTagName("input")[0].value;
-
+                    console.log(this.getElementsByTagName("input")[0]);
                     let name = database.chests[inp.value].id
                     let location = database.chests[inp.value].location
                     let x = location.split(", ")[0]
