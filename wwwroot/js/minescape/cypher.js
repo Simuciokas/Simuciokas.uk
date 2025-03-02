faddce03b10a8ec5a7656277a916052e7b8c0a03
Simuciokas <maxsimass@gmail.com>
2025-03-02 23:12:11 +0200

    Cypher requirement for 3 characters and show if multiple found + tip. Map tab show more based on browser height

--- wwwroot/js/minescape/cypher.js
+++ wwwroot/js/minescape/cypher.js
@@ -4,47 +4,68 @@ Startup();
 
 async function Startup() {
 
+    let seenIds = new Set();
+    let objects = Object.values(database.anagrams).filter(val => {
+        if (seenIds.has(val.id)) {
+            return false;
+        }
+        seenIds.add(val.id);
+        return true;
+    });
+
     document.getElementById("CypherInput").addEventListener("input", function (e, val) {
         let cipher = e.target.value.toLowerCase()
-        let ciphers = Object.keys(database.anagrams).filter(function (val) {
+        /*let ciphers = Object.keys(database.anagrams).filter(function (val) {
             return database.anagrams[val].id.length == cipher.length && database.anagrams[val].id.length > 0
-        })
+        })*/
 
-        let found = null;
-        for (let i = 1; i <= 26; i++) {
-            let shifted = caesarShift(cipher, i)
-            for (let j = 0; j < ciphers.length; j++) {
-                if (database.anagrams[ciphers[j]].id == shifted) {
-                    found = database.anagrams[ciphers[j]]
+        let foundList = null
+        if (cipher.length >= 3) {
+            for (let i = 1; i <= 26; i++) {
+                let shifted = caesarShift(cipher, i)
+                foundList = objects.filter(function (val) {
+                    return val.id === shifted || val.id.startsWith(shifted)
+                })
+                if (foundList.length >= 1) {
                     break
                 }
             }
         }
 
-        if (found == null) {
-            document.getElementById("solution-cypher-name").innerText = "not found"
+        if (foundList != null && foundList.length > 1) {
+            let listOfResults = foundList.map(x => x.id).join(", ")
+            document.getElementById("solution-cypher-name").innerHTML = `Found multiple: <b>${listOfResults}</b>. Type more characters to get the right result`
             document.getElementById("solution-cypher-tip").innerText = "not found"
             document.getElementById("solution-cypher-url").href = ""
             document.getElementById("solution-cypher-url").innerText = "not found"
             document.getElementById("solution-cypher-map").innerHTML = ""
         }
         else {
+            if (foundList == null || foundList.length == 0) {
+                document.getElementById("solution-cypher-name").innerText = "not found"
+                document.getElementById("solution-cypher-tip").innerText = "not found"
+                document.getElementById("solution-cypher-url").href = ""
+                document.getElementById("solution-cypher-url").innerText = "not found"
+                document.getElementById("solution-cypher-map").innerHTML = ""
+            }
+            else {
 
+                let found = foundList[0]
+                let name = found.id
+                let location = found.location
+                let x = location.split(", ")[0]
+                let y = location.split(", ")[1]
+                let z = location.split(", ")[2]
+                let tip = found.tip
 
-            let name = found.id
-            let location = found.location
-            let x = location.split(", ")[0]
-            let y = location.split(", ")[1]
-            let z = location.split(", ")[2]
-            let tip = found.tip
-
-            let text = `https://map.minescape.net/#/${x}/${y}/${z}/-2/minescape/minescape`
-            document.getElementById("solution-cypher-tip").innerText = tip
-            document.getElementById("solution-cypher-name").innerText = name == undefined ? "No name provided" : name
-            document.getElementById("solution-cypher-url").href = text
-            document.getElementById("solution-cypher-url").innerText = location
-            document.getElementById("solution-cypher-map").innerHTML = ""
-            document.getElementById("solution-cypher-map").innerHTML = `<iframe id=\"\" style=\"width:100%; height:650px;\" src=\"${text}\"></iframe>`
+                let text = `https://map.minescape.net/#/${x}/${y}/${z}/-2/minescape/minescape`
+                document.getElementById("solution-cypher-tip").innerText = tip
+                document.getElementById("solution-cypher-name").innerText = name == undefined ? "No name provided" : name
+                document.getElementById("solution-cypher-url").href = text
+                document.getElementById("solution-cypher-url").innerText = location
+                document.getElementById("solution-cypher-map").innerHTML = ""
+                document.getElementById("solution-cypher-map").innerHTML = `<iframe id=\"\" style=\"width:100%; height:650px;\" src=\"${text}\"></iframe>`
+            }
         }
     });
 }
