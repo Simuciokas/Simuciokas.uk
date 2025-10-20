72d1ed466aaa869d163d4da5d919ed8fbd60f4f7
Simuciokas <maxsimass@gmail.com>
2025-10-20 22:28:49 +0300

    Hide clickable url

--- wwwroot/js/map/overviewer.js
+++ wwwroot/js/map/overviewer.js
@@ -299,8 +299,7 @@ overviewer.util = {
 
         overviewer.map = L.map('mcmap', {crs: L.CRS.Simple});
 
-        overviewer.map.attributionControl.setPrefix(
-            '<a href="https://overviewer.org">Overviewer/Leaflet</a>');
+        overviewer.map.attributionControl.setPrefix('<a style="display: none;" href="https://overviewer.org">Overviewer/Leaflet</a>');
 
         overviewer.map.on('baselayerchange', function(ev) {
 
